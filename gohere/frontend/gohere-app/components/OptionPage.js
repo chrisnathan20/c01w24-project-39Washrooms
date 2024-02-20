0f47fd16e96f324fc21249caa957b448d4918b1d
Martin <martinloo498@gmail.com>
2024-02-20 18:34:29 -0500

    Init option page

---
+++ gohere/frontend/gohere-app/components/OptionPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const OptionPage = () => {
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  responseText: {
+    fontSize: 18,
+    color: '#333',
+  },
+});
+
+export default OptionPage;
