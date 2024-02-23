ed149e47fc4ee29a1d8a3a42606b7e8ada87b7a4
Martin <martinloo498@gmail.com>
2024-02-22 23:56:08 -0500

    Map options to buttons

--- gohere/frontend/gohere-app/components/OptionPage.js
+++ gohere/frontend/gohere-app/components/OptionPage.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const OptionPage = () => {
+  const options = [
+    {text: "Manage My Profile", destination: ""},
+    {text: "Add Publc Washroom", destination: ""},
+    {text: "Sign Up as a Business", destination: ""},
+    {text: "Log In as a Business", destination: ""},
+    {text: "Location Permission", destination: ""},
+    {text: "Privacy Policy", destination: ""},
+  ]
+  
+  const navigation = useNavigation();
+  
+  const handlePress = (dest) => {
+    if (dest != "") {
+      navigation.navigate(dest);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      
+      {options.map((el) => (
+        <Pressable onPress={handlePress(el.destination)}>
+          <Text>{el.text}</Text>
+        </Pressable>
+      ))}
     </View>
   );
 };
