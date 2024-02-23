79524a46321d29fdea16078d4fd19e37efb21caf
Frans Budiman <fransrewl@gmail.com>
2024-02-22 22:55:15 -0500

    surround NavBar with safeview, should work with IOS now

--- gohere/frontend/gohere-app/App.js
+++ gohere/frontend/gohere-app/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import TestConnection from './components/TestConnection';
+import TestConnection from './components/TestConnection'; //replace NavbarContainer with <TestConnection/> if you want to test connection
 import NavbarContainer from './components/NavbarContainer'
+import { SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavbarContainer />
+      <SafeAreaView style={{ flex: 1,  backgroundColor: "white"}}>
+       <NavbarContainer />
+      </SafeAreaView>
+      
       <StatusBar style="auto" />
     </View>
   );
