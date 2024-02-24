a8f4223edb4bcdeda162a2d35fa414c091992840
chrisnathan20 <christopher.nathanael@mail.utoronto.ca>
2024-02-23 23:29:52 -0500

    Update database schema and add mock washrooms for API development

--- gohere/backend/database.sql
+++ gohere/backend/database.sql
@@ -1,6 +1,4 @@
-DROP SCHEMA IF EXISTS gohere CASCADE;
-CREATE SCHEMA gohere;
-SET search_path TO gohere;
+-- CREATE DATABASE gohere;
 
 -- The BusinessOwners table consists of all the business owners who have chosen their washrooms to be in our GoHere system
 -- Businesses are identified by their unique email addresses
@@ -28,25 +26,24 @@ CREATE TABLE PlatinumBusiness (
     banner            BYTEA,
     FOREIGN KEY (email) REFERENCES BusinessOwners(email) ON DELETE RESTRICT);
 
--- The Location table contains all details about a business' location that has been submitted by the business 
--- when registering themselves into the GoHere system
-CREATE TABLE Location (
-    locationId          INTEGER PRIMARY KEY,
+-- The Washroom table contains all details about washrooms that has been submitted by the businesses and regular users
+CREATE TABLE Washrooms (
+    washroomId          INTEGER PRIMARY KEY,
     email	            VARCHAR(30),
-    locationName	    VARCHAR(30) NOT NULL,
+    washroomName	    VARCHAR(30) NOT NULL,
     longitude           DECIMAL NOT NULL,
     latitude            DECIMAL NOT NULL,
-    openingHour         TIME[7],
-    closingHour         TIME[7],
+    openingHours         TIME[7],
+    closingHours         TIME[7],
     address1            VARCHAR(100) NOT NULL,
     address2            VARCHAR(100),
-    postalCode          VARCHAR(10) NOT NULL
+    postalCode          VARCHAR(10) NOT NULL,
     FOREIGN KEY (email) REFERENCES BusinessOwners(email) ON DELETE RESTRICT);
 
 -- The Report table contains all reports submitted by GoHere app users
 CREATE TABLE Report (
     reportId            INTEGER PRIMARY KEY,
-    locationId          INTEGER NOT NULL REFERENCES Location(locationId) ON DELETE RESTRICT,
+    locationId          INTEGER NOT NULL REFERENCES Washrooms(washroomId) ON DELETE RESTRICT,
     reason	            INTEGER NOT NULL,
     reportTime          TIMESTAMP NOT NULL);
 
@@ -58,15 +55,15 @@ CREATE TABLE BusinessApplication (
     status              INTEGER NOT NULL, 
     longitude           DECIMAL NOT NULL UNIQUE,
     latitude            DECIMAL NOT NULL UNIQUE,
-    openingHour         TIME[7],
-    closingHour         TIME[7],
+    openingHours         TIME[7],
+    closingHours         TIME[7],
     address1            VARCHAR(100) NOT NULL,
     address2            VARCHAR(100),
     postalCode          VARCHAR(10) NOT NULL,
     additionalDetails   VARCHAR(100),
     imageOne            BYTEA,
     imageTwo            BYTEA,
-    imageThree          BYTEA
+    imageThree          BYTEA,
     FOREIGN KEY (email) REFERENCES BusinessOwners(email) ON DELETE RESTRICT);
 
 -- The PublicApplication are all the applications GoHere app users have submitted to register a washroom into the GoHere system       
@@ -76,8 +73,8 @@ CREATE TABLE PublicApplication (
     status              INTEGER NOT NULL,
     longitude           DECIMAL NOT NULL UNIQUE,
     latitude            DECIMAL NOT NULL UNIQUE,
-    openingHour         TIME[7],
-    closingHour         TIME[7],
+    openingHours         TIME[7],
+    closingHours         TIME[7],
     address1            VARCHAR(100) NOT NULL,
     address2            VARCHAR(100),
     postalCode          VARCHAR(10),
@@ -94,3 +91,37 @@ CREATE TABLE testconnection(
 
 INSERT INTO testconnection (connectionstatus) VALUES ('Successfully setup admin app');
 INSERT INTO testconnection (connectionstatus) VALUES ('Successfully setup user app');
+
+-- mock database entries for API development
+INSERT INTO BusinessOwners (email, password, businessName, sponsorship) 
+VALUES ('mock@business.com', '1', 'Mock Business', 'Basic');
+
+INSERT INTO Washrooms (washroomId, email, washroomName, longitude, latitude, openingHours, closingHours, address1, postalCode) 
+VALUES (1, 'mock@business.com', 'Mock Washroom 1', -79.255925, 43.773509, 
+    ARRAY[TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00'], 
+    ARRAY[TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59'], 
+    '123 Mock Street', 'M0C 0K1');
+
+INSERT INTO Washrooms (washroomId, email, washroomName, longitude, latitude, openingHours, closingHours, address1, postalCode) 
+VALUES (2, 'mock@business.com', 'Mock Washroom 2', -79.258508, 43.777109, 
+    ARRAY[TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00'], 
+    ARRAY[TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59'], 
+    '456 Mock Drive', 'M0C 0K2');
+
+INSERT INTO Washrooms (washroomId, email, washroomName, longitude, latitude, openingHours, closingHours, address1, postalCode)
+VALUES (3, 'mock@business.com', 'Mock Washroom 3', -79.257084, 43.772374, 
+    ARRAY[TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00'], 
+    ARRAY[TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59'], 
+    '789 Mock Hallway', 'M0D 0K3');
+
+INSERT INTO Washrooms (washroomId, email, washroomName, longitude, latitude, openingHours, closingHours, address1, postalCode) 
+VALUES (4, 'mock@business.com', 'Mock Washroom 4', -79.260508, 43.780709, 
+    ARRAY[TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00'], 
+    ARRAY[TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59'], 
+    '101 Mock Avenue', 'M0E 0K4');
+
+INSERT INTO Washrooms (washroomId, email, washroomName, longitude, latitude, openingHours, closingHours, address1, postalCode) 
+VALUES (5, 'mock@business.com', 'Mock Washroom 5', -79.264308, 43.794309, 
+    ARRAY[TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00', TIME '00:00:00'], 
+    ARRAY[TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59', TIME '23:59:59'], 
+    '202 Mock Lane', 'M0F 0K5');
