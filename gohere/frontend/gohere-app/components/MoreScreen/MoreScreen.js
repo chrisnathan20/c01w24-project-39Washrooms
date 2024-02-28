4e17696119175affaf93179232def5ce9e1de145
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2024-02-28 15:18:11 -0500

    changed padding

--- gohere/frontend/gohere-app/components/MoreScreen/MoreScreen.js
+++ gohere/frontend/gohere-app/components/MoreScreen/MoreScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import MoreDefaultPage from './MoreDefaultPage';
@@ -7,28 +8,36 @@ const MoreScreen = () => {
   const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer independent={true}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="More Options..."
-          component={MoreDefaultPage}
-          options={{
-            headerTitleStyle: {
-              fontStyle: 'normal',
-              fontWeight: 700,
-              paddingTop: 20,
-              paddingLeft: 10,
-              fontSize: 30,
-              color: '#DA5C59'
-            },
-            headerShadowVisible: false,
-            cardStyle: { backgroundColor: '#FFFFFF' }
-          }}
-        />
-        {/* Add a new <Stack.Screen> here when making new page. Also add onPress to MoreDefaultPage.js */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <NavigationContainer independent={true}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="More Options..."
+            component={MoreDefaultPage}
+            options={{
+              headerTitleStyle: {
+                fontStyle: 'normal',
+                fontWeight: 700,
+                paddingLeft: 10,
+                fontSize: 30,
+                color: '#DA5C59'
+              },
+              headerShadowVisible: false,
+              cardStyle: { backgroundColor: '#FFFFFF' }
+            }}
+          />
+          {/* Add a new <Stack.Screen> here when making new page. Also add onPress to MoreDefaultPage.js */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+});
 
 export default MoreScreen;
