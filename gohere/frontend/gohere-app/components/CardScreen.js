aace4bb7f377df6fb1fb9ae110d6d77fb5b1a9e8
JamieHvizdos <114103851+JamieHvizdos@users.noreply.github.com>
2024-02-29 14:24:45 -0500

    *NOT WORKING*  Implemented Review Popup
    The code does not work but follows the steps outlined: https://github.com/KjellConnelly/react-native-rate
    
    To run, first run npm install react-native-rate.
    When you click on the CardScreen page, it should bring up the review pop-up. If there is no valid address for the review store, it should redirect the user to the crohns website.
    
    The error occurs with the Rate.rate method. The error message says "Cannot read property 'rate' of null"

--- gohere/frontend/gohere-app/components/CardScreen.js
+++ gohere/frontend/gohere-app/components/CardScreen.js
@@ -1,9 +1,46 @@
-import { View, Text } from 'react-native';
+import React, {useEffect, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import Rate, { AndroidMarket } from 'react-native-rate';
+
 
 export default function CardScreenTest() {
-    return (
-        <View style ={{justifyContent: 'center', alignItems: 'center', flex:1}}>
-            <Text>Card Screen</Text>
-        </View>
-    );
+    //const [rated, setRated] = useState(false);
+
+    const showReviewPopUp = () => {
+      const options = {
+        AppleAppID: "1011069090",
+        GooglePackageName: "com.GoHere.GoHere",
+        AmazonPackageName: "com.GoHere.GoHere",
+        OtherAndroidURL: "https://play.google.com/store/apps/details?id=com.GoHere.GoHere&hl=en&gl=US",
+        preferredAndroidMarket: AndroidMarket.Google,
+        preferInApp: true,
+        openAppStoreIfInAppFails: true,
+        fallbackPlatformURL: "https://crohnsandcolitis.ca/",
+      };
+      
+      Rate.rate(options, (success, errorMessage) => {
+        if (success) {
+          // this technically only tells us if the user successfully went to the Review Page. Whether they actually did anything, we do not know.
+          setRated(true);
+        }
+        if (errorMessage) {
+          // errorMessage comes from the native code. Useful for debugging, but probably not for users to view
+          console.error(`Rate.rate() error: ${errorMessage}`);
+        }
+      });
+      
+    };
+    
+      useEffect(() => {
+        // Show review pop-up only if the user hasn't rated the app yet
+          showReviewPopUp();
+      });
+
+
+
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>Card Screen</Text>
+    </View>
+  );
 }
