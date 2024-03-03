31913a96860e32be9ae2033c670c87bf6fd7ee85
ashvini8 <ashvinihunagund@gmail.com>
2024-03-03 01:05:25 -0500

    french version of card

---
+++ gohere/frontend/gohere-app/components/AccessCardFr.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function AccessCard() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [disease, setDisease] = useState('None');
+
+    useEffect(() => {
+        // Fetch the stored data when the component mounts
+        const fetchData = async () => {
+            try {
+                const storedFirstName = await AsyncStorage.getItem('firstName');
+                const storedLastName = await AsyncStorage.getItem('lastName');
+                const storedDisease = await AsyncStorage.getItem('disease');
+
+                if (storedFirstName !== null) {
+                    setFirstName(storedFirstName);
+                }
+                if (storedLastName !== null) {
+                    setLastName(storedLastName);
+                }
+                if (storedDisease !== null) {
+                    setDisease(storedDisease);
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+        
+    }, []);
+
+    const renderDiseaseText = () => {
+        if (disease === "Crohn's disease") {
+            return 'Maladie de Crohn';
+        } else if (disease === "Ulcerative colitis") {
+            return 'Colite ulcéreuse';
+        } else {
+            return disease
+        }
+    };
+
+    return (
+        <View style={styles.cardContainer}>
+            <Image style={styles.cardImage} source={require('../assets/card-image.jpeg')} resizeMode="cover"/>
+            <View style={styles.textContainer}>
+                <Text style={[styles.subheading_text, {fontWeight: 'bold'}]}>Toilette</Text>
+                <Text style={[styles.heading_text]}>Carte d'accès</Text>
+                <View style={styles.outerBorder}>
+                    <Text style={[styles.paragraph_text, styles.borderedText]}>{renderDiseaseText()}</Text>
+                </View>
+                <Text style={[styles.subheading_text, {fontSize: 20}]}>{firstName} name {lastName}</Text>
+                <Text style={styles.paragraph_text}>Veuilliez m'aider. Je nécessite un accés urgent aux toilettes.</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        backgroundColor: '#DA5C59',
+        flexDirection: 'row',
+        borderRadius: 8,
+        shadowColor: '#000',
+        shadowOffset: {width: 4, height: 4},
+        shadowOpacity: 0.7,
+        shadowRadius: 5,
+        elevation: 5, // Add this line for Android shadows
+    },
+    textContainer: {
+        flex: 1,
+        //justifyContent:'center',
+        padding: 15,
+    },
+    borderedText: {
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8,
+        margin: 15,
+        padding: 10,
+        
+
+    },
+
+    outerBorder: {
+        alignSelf: 'flex-start',
+    },
+
+    cardImage: {
+        height: '100%',
+        width: 100,
+        borderBottomLeftRadius: 8,
+        borderTopLeftRadius: 8,
+    },
+    heading_text: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'left',
+    },
+    paragraph_text: {
+        fontSize: 15,
+        color: 'white',
+        textAlign: 'left',
+    },
+    subheading_text: {
+        fontSize: 15,
+        color: 'white',
+        textAlign: 'left',
+        marginTop: 5,
+        marginBottom: 5,
+    },
+
+});
+
--- gohere/frontend/gohere-app/components/CardScreen.js
+++ gohere/frontend/gohere-app/components/CardScreen.js
@@ -1,15 +1,51 @@
-import { View, Text, StyleSheet, Switch } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, LayoutAnimation } from 'react-native';
 import AccessCard from './AccessCard';
+import AccessCardFr from './AccessCardFr';
 
 export default function CardScreenTest() {
+    const [isFrench, setisFrench] = React.useState(false);
+    const activeColor = '#DA5C59';
+    const inactiveColor = '#EDEDED'; // Grey color
+
+    const handleToggle = () => {
+        LayoutAnimation.easeInEaseOut();
+        setisFrench(!isFrench);
+    };
 
     return (
         <View style={styles.cardPageContainer}>
-            <Text style={[styles.heading_text]}>Access Card</Text>
-            <AccessCard/>
-        </View>
+            <View style={styles.cardHeader}>
+            <Text style={styles.heading_text}>{isFrench ? "Carte d'accès" : 'Access Card'}</Text>
 
-        
+                <TouchableOpacity
+                    style={styles.toggle}
+                    onPress={handleToggle}>
+                        
+                    <View style={[
+                        styles.toggleInView,
+                        {backgroundColor: isFrench ? inactiveColor : activeColor,
+                        justifyContent: 'center'}
+                    ]}>
+                        <Text 
+                            style={[styles.toggleLabel, 
+                            { color: isFrench ? '#DA5C59' : 'white' }]}>
+                            en</Text>
+                    
+                    </View>
+
+                    <View style={[
+                        styles.toggleInView,
+                        {backgroundColor: isFrench ? activeColor : inactiveColor,
+                            justifyContent: 'center'}
+                    ]}>
+                        <Text style={[styles.toggleLabel, { color: isFrench ? 'white' : '#DA5C59' }]}>fr</Text>
+                    
+                    </View>
+                </TouchableOpacity>
+            </View>
+            {isFrench ? <AccessCardFr /> : <AccessCard />}
+        </View>
     );
 }
 
@@ -20,15 +56,39 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
         paddingTop: 40,
     },
+    cardHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+    },
     heading_text: {
-        fontSize: 25,
+        fontSize: 30,
         fontWeight: 'bold',
         color: '#DA5C59',
         textAlign: 'left',
-        margin: 25,
+        marginTop: 25,
+        marginBottom: 25,
     },
-
-    label: {
-    marginRight: 10,
+    toggle: {
+        height: 40,
+        width: 120,
+        borderRadius: 5,
+        borderWidth: 5,
+        borderColor: '#EDEDED',
+        backgroundColor: '#EDEDED',
+        overflow: 'hidden',
+        flexDirection: 'row',
+        alignItems: 'stretch', // Adjusted to stretch items to fill the height
+    },
+    toggleInView: {
+        flex: 1, // Equal flex for both views to take half of the TouchableOpacity
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+    },
+    toggleLabel: {
+        fontSize: 16,
+        fontWeight: 'bold',
     },
 });
