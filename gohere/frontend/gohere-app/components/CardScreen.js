cf5be2d3c6c981d4680a35827ea1c862d10bf853
ashvini8 <ashvinihunagund@gmail.com>
2024-03-02 21:06:36 -0500

    Access Card Component

--- gohere/frontend/gohere-app/components/CardScreen.js
+++ gohere/frontend/gohere-app/components/CardScreen.js
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Switch } from 'react-native';
+import AccessCard from './AccessCard';
 
 export default function CardScreenTest() {
+
     return (
-        <View style ={{justifyContent: 'center', alignItems: 'center', flex:1}}>
-            <Text>Card Screen</Text>
+        <View style={styles.cardPageContainer}>
+            <Text style={[styles.heading_text]}>Access Card</Text>
+            <AccessCard/>
         </View>
+
+        
     );
 }
+
+const styles = StyleSheet.create({
+    cardPageContainer: {
+        backgroundColor: '#fff',
+        flex: 1,
+        paddingHorizontal: 30,
+        paddingTop: 40,
+    },
+    heading_text: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#DA5C59',
+        textAlign: 'left',
+        margin: 25,
+    },
+
+    label: {
+    marginRight: 10,
+    },
+});
