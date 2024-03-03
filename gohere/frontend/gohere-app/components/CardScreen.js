1f7eb85d3801ca96b904da75a29a22ef1a05d9c0
JamieHvizdos <114103851+JamieHvizdos@users.noreply.github.com>
2024-03-02 20:12:47 -0500

    Revert CardScreen.js back to original state

--- gohere/frontend/gohere-app/components/CardScreen.js
+++ gohere/frontend/gohere-app/components/CardScreen.js
@@ -1,46 +1,9 @@
-import React, {useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import Rate, { AndroidMarket } from 'react-native-rate';
-
+import { View, Text } from 'react-native';
 
 export default function CardScreenTest() {
-    //const [rated, setRated] = useState(false);
-
-    const showReviewPopUp = () => {
-      const options = {
-        AppleAppID: "1011069090",
-        GooglePackageName: "com.GoHere.GoHere",
-        AmazonPackageName: "com.GoHere.GoHere",
-        OtherAndroidURL: "https://play.google.com/store/apps/details?id=com.GoHere.GoHere&hl=en&gl=US",
-        preferredAndroidMarket: AndroidMarket.Google,
-        preferInApp: true,
-        openAppStoreIfInAppFails: true,
-        fallbackPlatformURL: "https://crohnsandcolitis.ca/",
-      };
-      
-      Rate.rate(options, (success, errorMessage) => {
-        if (success) {
-          // this technically only tells us if the user successfully went to the Review Page. Whether they actually did anything, we do not know.
-          setRated(true);
-        }
-        if (errorMessage) {
-          // errorMessage comes from the native code. Useful for debugging, but probably not for users to view
-          console.error(`Rate.rate() error: ${errorMessage}`);
-        }
-      });
-      
-    };
-    
-      useEffect(() => {
-        // Show review pop-up only if the user hasn't rated the app yet
-          showReviewPopUp();
-      });
-
-
-
-  return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Card Screen</Text>
-    </View>
-  );
+    return (
+        <View style ={{justifyContent: 'center', alignItems: 'center', flex:1}}>
+            <Text>Card Screen</Text>
+        </View>
+    );
 }
