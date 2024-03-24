8a4bffb8e6c6debbe6238dcaf303a9f8c732e7d9
Syeda Sumaiya Ahmed <114104419+ssumaiyaahmed@users.noreply.github.com>
2024-03-24 19:21:19 -0400

    Create adminNewsCardImage.js

---
+++ gohere/frontend/gohere-admin-app/components/AdminNewsScreens/adminNewsCardImage.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image } from 'react-native';
+import { GOHERE_SERVER_URL, GOOGLE_API_KEY } from '../../env.js';
+
+const cardImage = ({ newsId,givenStyle }) => {
+
+  const [imageUri, setImageUri] = useState(null);
+  const [error, setError] = useState(null);
+
+  // to fetch image from backend for testing
+  useEffect(() => {
+    fetchImageUrl = async () => {
+      try {
+        const response = await fetch(`${GOHERE_SERVER_URL}/newsCardImage/${newsId}`);
+
+        if (!response.ok) {
+          throw new Error('Server responded with an error.');
+        }
+        
+        const imagePath = await response.text();
+
+        const imageUrl = `${GOHERE_SERVER_URL}/${imagePath}`;
+        //console.log(newsId, imageUrl)
+
+        setImageUri(imageUrl);
+
+      } catch (error) {
+        console.error('Error fetching image URLs:', error);
+      }
+
+      //setTimeout(fetchImageUrl, 7000);
+    };
+
+
+    fetchImageUrl();
+  }, [newsId]);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {error ? (
+        <Text>{error}</Text>
+      ) : (
+        <Image source={{ uri: imageUri }} style={givenStyle}/>//width: 110, height: 110, bottom: 73, borderRadius: 15, marginLeft: 128, }} />
+      )}
+    </View>
+  );
+};
+export default cardImage
