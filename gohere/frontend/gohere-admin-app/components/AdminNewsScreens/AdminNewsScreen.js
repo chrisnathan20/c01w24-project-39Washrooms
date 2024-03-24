627f6ece54d3e847f8bcdf7be445d6a20ec8a04b
Syeda Sumaiya Ahmed <114104419+ssumaiyaahmed@users.noreply.github.com>
2024-03-24 19:18:12 -0400

    Create AdminNewsScreen.js

---
+++ gohere/frontend/gohere-admin-app/components/AdminNewsScreens/AdminNewsScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NewsListScreen from './NewsListScreen';
+import AddNews from './AddNews';
+import UpdateNews from './UpdateNews';
+
+const Stack = createStackNavigator();
+
+const AdminNewsScreen = () => {
+
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="NewsListScreen">
+        <Stack.Screen name="NewsList" component={NewsListScreen} options={{ title: 'NewsListScreen', headerShown: false }} />
+        <Stack.Screen name="Add" component={AddNews} options={{ title: 'AddNews', headerShown: false }} />
+        <Stack.Screen name="Update" component={UpdateNews} options={{ title: 'UpdateNews', headerShown: false }} />      
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AdminNewsScreen;
