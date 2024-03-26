934acf8b4d58c19abeef3605d7d2f315970f5986
Syeda Sumaiya Ahmed <114104419+ssumaiyaahmed@users.noreply.github.com>
2024-03-25 22:18:36 -0400

    Update adminNewsCardImage.js with useFocusEffect

--- gohere/frontend/gohere-admin-app/components/AdminNewsScreens/adminNewsCardImage.js
+++ gohere/frontend/gohere-admin-app/components/AdminNewsScreens/adminNewsCardImage.js
@@ -1,48 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image } from 'react-native';
-import { GOHERE_SERVER_URL, GOOGLE_API_KEY } from '../../env.js';
+import { GOHERE_SERVER_URL } from '../../env.js';
+import { useFocusEffect } from '@react-navigation/native'; // Import useFocusEffect
 
-const cardImage = ({ newsId,givenStyle }) => {
-
+const CardImage = ({ newsId, givenStyle }) => {
   const [imageUri, setImageUri] = useState(null);
   const [error, setError] = useState(null);
 
-  // to fetch image from backend for testing
-  useEffect(() => {
-    fetchImageUrl = async () => {
-      try {
-        const response = await fetch(`${GOHERE_SERVER_URL}/newsCardImage/${newsId}`);
+  // Use useFocusEffect to fetch image when the screen is focused
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchImageUrl = async () => {
+        try {
+          const response = await fetch(`${GOHERE_SERVER_URL}/newsCardImage/${newsId}`);
 
-        if (!response.ok) {
-          throw new Error('Server responded with an error.');
+          if (!response.ok) {
+            console.log('Server responded with an error.');
+          }
+          
+          const imagePath = await response.text();
+
+          const imageUrl = `${GOHERE_SERVER_URL}/${imagePath}`;
+          setImageUri(imageUrl);
+        } catch (error) {
+          console.error('Error fetching image URLs:', error);
+          setError(error.message);
         }
-        
-        const imagePath = await response.text();
+      };
 
-        const imageUrl = `${GOHERE_SERVER_URL}/${imagePath}`;
-        //console.log(newsId, imageUrl)
-
-        setImageUri(imageUrl);
-
-      } catch (error) {
-        console.error('Error fetching image URLs:', error);
-      }
-
-      //setTimeout(fetchImageUrl, 7000);
-    };
-
-
-    fetchImageUrl();
-  }, [newsId]);
+      fetchImageUrl(); 
+    }, [newsId]) 
+  );
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       {error ? (
         <Text>{error}</Text>
       ) : (
-        <Image source={{ uri: imageUri }} style={givenStyle}/>//width: 110, height: 110, bottom: 73, borderRadius: 15, marginLeft: 128, }} />
+        <Image source={{ uri: imageUri }} style={givenStyle}/>
       )}
     </View>
   );
 };
-export default cardImage
+
+export default CardImage;
