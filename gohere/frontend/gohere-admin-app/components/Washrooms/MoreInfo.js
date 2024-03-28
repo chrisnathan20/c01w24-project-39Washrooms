4501c4257a25adbe13d544da6d9726afc7a05950
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2024-03-27 21:14:29 -0400

    added font imports

--- gohere/frontend/gohere-admin-app/components/Washrooms/MoreInfo.js
+++ gohere/frontend/gohere-admin-app/components/Washrooms/MoreInfo.js
@@ -50,6 +50,7 @@ const MoreInfo = ({ navigation, route }) => {
     const [fontsLoaded, fontError] = useFonts({
         'Poppins-Regular': require('../../assets/fonts/Poppins-Regular.ttf'),
         'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+        'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
         'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf')
       });
 
